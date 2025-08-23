6eda64458aa2ce7319eff19053f60a981b0d52cf
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-23 15:18:06 +0700

    bai_13

---
+++ Buoi_01/dist/bai_13.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Circle = exports.Square = exports.Shape = void 0;
+// 13. Create an abstract class Shape with method area(). Implement Square and Circle.
+class Shape {
+    area() { }
+}
+exports.Shape = Shape;
+class Square {
+    area() { }
+}
+exports.Square = Square;
+class Circle {
+    area() { }
+}
+exports.Circle = Circle;
---
+++ Buoi_01/src/bai_13.ts
@@ -0,0 +1,11 @@
+// 13. Create an abstract class Shape with method area(). Implement Square and Circle.
+export abstract class Shape {
+  area() {}
+}
+export class Square implements Shape {
+  area(): void {}
+}
+
+export class Circle implements Shape {
+  area(): void {}
+}
