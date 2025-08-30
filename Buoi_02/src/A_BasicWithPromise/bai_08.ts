409584f7386d67fd4d31f5dc1374f429932b6696
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 13:22:04 +0700

    bai_08

---
+++ Buoi_02/src/A_BasicWithPromise/bai_08.ts
@@ -0,0 +1,15 @@
+// 8. Create a Promise chain: square the number 2, then double it, then add 5.
+const chain = Promise.resolve(2)
+  .then((num) => {
+    return num * num;
+  })
+  .then((num) => {
+    return num * 2;
+  })
+  .then((num) => {
+    return num + 5;
+  });
+
+chain.then((result) => {
+  console.log("Result: ", result);
+});
