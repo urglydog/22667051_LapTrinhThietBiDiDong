7399b8aa01dd5df06a00ca29492e33c541375397
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 12:56:07 +0700

    cau_03

---
+++ Buoi_02/dist/A_BasicWithPromise/bai_03.js
@@ -0,0 +1,11 @@
+"use strict";
+// 3. Write a function that rejects a Promise with the error "Something went wrong" after 1
+// second.
+const rejectPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject("Something went wrong");
+    }, 1000);
+});
+rejectPromise.catch((err) => {
+    console.error(err);
+});
