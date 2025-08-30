4110508c707835af97bd4e2761dc925549da3459
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 16:40:29 +0700

    bai_28

---
+++ Buoi_02/dist/C_FetchAPI_Simulated_IO/bai_28.js
@@ -0,0 +1,28 @@
+"use strict";
+// 28. Write an async function batchProcess() that processes 5 async tasks at once (use
+// Promise.all).
+// Hàm giả lập 1 task async
+function asyncTask(id, time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Task ${id} completed in ${time} ms`);
+        }, time);
+    });
+}
+// Hàm batchProcess xử lý 5 tasks song song
+async function batchProcess() {
+    const tasks = [
+        asyncTask(1, 1000),
+        asyncTask(2, 1500),
+        asyncTask(3, 2000),
+        asyncTask(4, 2500),
+        asyncTask(5, 3000),
+    ];
+    const results = await Promise.all(tasks);
+    return results;
+}
+// Gọi hàm
+batchProcess().then((res) => {
+    console.log("All tasks done:");
+    console.log(res);
+});
