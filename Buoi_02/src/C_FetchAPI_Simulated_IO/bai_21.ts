603dbb4f927d04506add2365454284d5712b9c77
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 16:21:16 +0700

    bai_21

---
+++ Buoi_02/src/C_FetchAPI_Simulated_IO/bai_21.ts
@@ -0,0 +1,11 @@
+// 21. Use fetch to get data from a public API (e.g.,
+// https://jsonplaceholder.typicode.com/todos/1).
+async function fetchPublic() {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+  const jsonData = await data.json();
+  return jsonData;
+}
+
+fetchPublic().then((jsonData) => {
+  console.log(jsonData);
+});
