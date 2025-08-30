0767682fe43c9e232d5c962357b8fd27c773f74f
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 16:30:59 +0700

    bai_23

---
+++ Buoi_02/src/C_FetchAPI_Simulated_IO/bai_23.ts
@@ -0,0 +1,11 @@
+// 23. Write an async function that fetches a list of todos and filters out those that are not
+// completed.
+async function fetch_Fillter() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const jsondata = await res.json();
+  return jsondata.filter((todo: any) => todo.completed === false);
+}
+
+fetch_Fillter().then((result) => {
+  console.log(result);
+});
