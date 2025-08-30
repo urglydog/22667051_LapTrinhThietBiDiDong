047645160bc04b40cb810f3d2b121de1069d54b8
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 15:47:27 +0700

    bai_17

---
+++ Buoi_02/src/B_AsyncAwait/bai_17.ts
@@ -0,0 +1,15 @@
+// 17. Use for await...of to iterate over an array of Promises.
+
+async function demoForAwaitOf() {
+  const promises = [
+    Promise.resolve("A"),
+    Promise.resolve("B"),
+    Promise.resolve("C"),
+  ];
+
+  for await (const value of promises) {
+    console.log(value);
+  }
+}
+
+demoForAwaitOf();
