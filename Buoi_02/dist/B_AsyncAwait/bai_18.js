c101b8bfeb5ec2f656d9db3b50cd7b024ec03641
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 15:55:30 +0700

    bai_18

---
+++ Buoi_02/dist/B_AsyncAwait/bai_18.js
@@ -0,0 +1,11 @@
+"use strict";
+// 18. Write an async function fetchUser(id) that simulates an API call (resolves a user
+// object after 1 second).
+async function fetchUser(id) {
+    const api_data = await fetch(`https://6819db9a1ac115563506bd6e.mockapi.io/users/${id}`);
+    const users = api_data.json();
+    return users;
+}
+fetchUser("1").then((result) => {
+    console.log(result);
+});
