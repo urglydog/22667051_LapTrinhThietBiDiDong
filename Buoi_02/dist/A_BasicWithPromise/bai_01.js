c22c8c4527a94401be8fe68fc46af3f8c4bd4cbc
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 12:52:32 +0700

    cau_01 cau_02

---
+++ Buoi_02/dist/A_BasicWithPromise/bai_01.js
@@ -0,0 +1,11 @@
+"use strict";
+// 1. Create a Promise that returns the string "Hello Async" after 2 seconds.
+const helloAsyncPromise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Hello Async");
+    }, 2000);
+});
+// Example usage:
+helloAsyncPromise.then((result) => {
+    console.log(result); // "Hello Async" (sau 2 giây)
+});
---
+++ Buoi_02/src/A_BasicWithPromise/bai_02.ts
@@ -0,0 +1,10 @@
+// 2. Write a function that returns a Promise resolving with the number 10 after 1 second.
+const numberAsync = new Promise<number>((resolve) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 1000);
+});
+
+numberAsync.then((result) => {
+  console.log(result);
+});
