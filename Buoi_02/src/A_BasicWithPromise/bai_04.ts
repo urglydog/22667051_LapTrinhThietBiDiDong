73e9d7a0c488a59dc2a8da62f581a0e5030a63f3
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 13:03:11 +0700

    bai_01 bai_02 bai_03 bai_04

---
+++ Buoi_02/src/A_BasicWithPromise/bai_04.ts
@@ -0,0 +1,20 @@
+// 4. Use .then() and .catch() to handle a Promise that returns a random number.
+const randomPromise = new Promise<number>((resolve, reject) => {
+  const random = Math.random();
+
+  setTimeout(() => {
+    if (random > 0.2) {
+      resolve(random); // thành công (80% cơ hội)
+    } else {
+      reject("Random number too small!"); // thất bại (20% cơ hội)
+    }
+  }, 1000);
+});
+
+randomPromise
+  .then((result) => {
+    console.log("Random number:", result);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
