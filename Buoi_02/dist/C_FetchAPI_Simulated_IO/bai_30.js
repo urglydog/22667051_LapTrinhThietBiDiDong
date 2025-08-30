a472fe5720f5b2ea3898bac950a644bb8ebc41db
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-08-30 16:44:47 +0700

    bai_30

---
+++ Buoi_02/dist/C_FetchAPI_Simulated_IO/bai_30.js
@@ -0,0 +1,27 @@
+"use strict";
+// 30. Use async/await + Promise.allSettled() to handle multiple API calls and display their
+// success/failure status.
+async function fetchUrl(url) {
+    const res = await fetch(url);
+    if (!res.ok)
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    return res.json();
+}
+async function runMultipleApis() {
+    const urls = [
+        "https://jsonplaceholder.typicode.com/todos/1", // hợp lệ
+        "https://jsonplaceholder.typicode.com/todos/2", // hợp lệ
+        "https://jsonplaceholder.typicode.com/invalid", // sai -> lỗi 404
+    ];
+    const results = await Promise.allSettled(urls.map((url) => fetchUrl(url)));
+    results.forEach((result, index) => {
+        if (result.status === "fulfilled") {
+            console.log(`✅ API ${index + 1} success:`, result.value);
+        }
+        else {
+            console.log(`❌ API ${index + 1} failed:`, result.reason.message);
+        }
+    });
+}
+// Gọi hàm
+runMultipleApis();
